808c133cb3189b8bb8248346dca90965ef377afe
SumedhDawadi <sumedh,dawadi101@gmail.com>
2021-03-24 19:10:26 +0545

    TCP_ChatRoom (Client)

--- start.py
+++ start.py
@@ -1,26 +1,55 @@
-import threading
 import socket
-
-hosts = '127.0.0.1' #Loopback address
+import threading
+host = '127.0.0.1'
 port = 55555
+
+# Starting Server
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(hosts,port)
+server.bind((host, port))
 server.listen()
+
+# Lists For Clients and Their Nicknames
 clients = []
 nicknames = []
-
 def broadcast(message):
-    for clients in clients:
-        clients.send(message)
-def handle(clients):
+    for client in clients:
+        client.send(message)
+def handle(client):
     while True:
         try:
-            message = clients.recv(1024)
+            # Broadcasting Messages
+            message = client.recv(1024)
             broadcast(message)
         except:
-            index = clients.index(clients)
-            clients.remove(clients)
-            nicknames = nicknames[index]
-            broadcast(f'(nicknams) Left the chat room'.encode('ascii'))
-            nicknames.remove(nicknames)
+            # Removing And Closing Clients
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast('{} Left the room!'.format(nickname).encode('ascii'))
+            nicknames.remove(nickname)
             break
+# Receiving / Listening Function
+def receive():
+    while True:
+        # Accept Connection
+        client, address = server.accept()
+        print("Connected with {}".format(str(address)))
+
+        # Request And Store Nickname
+        client.send('ConnectME'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+
+        # Print And Broadcast Nickname
+        print("Nickname is {}".format(nickname))
+        broadcast("{} Joined the room !".format(nickname).encode('ascii'))
+        client.send('Connected to server!'.encode('ascii'))
+
+        # Start Handling Thread For Client
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+
+receive()
